4a2c20ec191e55bef79ca9442ad99ccf7378b7b8
javierpb275 <javierpb275@gmail.com>
2024-03-23 12:10:29 +0100

    added background color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 73, 141, 204),
         body: Center(
           child: Text('Hello World!'),
         ),
