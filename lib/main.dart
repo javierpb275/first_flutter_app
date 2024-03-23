332e7f7289a3b70026b0468d3fe70317936d3b9b
javierpb275 <javierpb275@gmail.com>
2024-03-23 12:32:23 +0100

    How To Configure Widgets & Objects. Set begin and end of gradient background color.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [Colors.deepPurple, Color.fromARGB(255, 138, 108, 196)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const Center(
