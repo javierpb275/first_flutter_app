71a0d6bd1dd91c80c83ecc6d1e86609afee4cc73
javierpb275 <javierpb275@gmail.com>
2024-03-24 16:16:02 +0100

    changed test to remove errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:first_flutter_app/gradient_container.dart';
+import 'package:first_flutter_app/styled_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:first_flutter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GradientContainer(
+      colors: [],
+      child: StyledText(''),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
