d83ffa8109384225b9a0e853e0e219e4094b47e8
javierpb275 <javierpb275@gmail.com>
2024-03-24 14:06:38 +0100

    introducing variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,17 +1,20 @@
 import 'package:first_flutter_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepPurple, Color.fromARGB(255, 138, 108, 196)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: const [Colors.deepPurple, Color.fromARGB(255, 138, 108, 196)],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyledText()),
