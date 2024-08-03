e2810c70f766b23cfcc2503bd967f15027fc0b1f
javierpb275 <javierpb275@gmail.com>
2024-08-03 12:35:49 +0200

    creatng a fullscreen modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -72,6 +72,7 @@ class _NewExpenseState extends State<NewExpense> {
         amount: enteredAmount,
       ),
     );
+    Navigator.pop(context);
   }
 
   @override
@@ -84,7 +85,12 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(
+        16,
+        48,
+        16,
+        16,
+      ),
       child: Column(
         children: [
           TextField(
