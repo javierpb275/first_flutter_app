1223df4bf66c4cb89ecd7a2447c93d3a66fb39bf
javierpb275 <javierpb275@gmail.com>
2024-08-03 13:17:49 +0200

    Using the Dismissible Widget for Dismissing List Items

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -34,6 +34,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       isScrollControlled: true,
@@ -62,7 +68,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -2,19 +2,38 @@ import 'package:first_flutter_app/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 import 'package:first_flutter_app/models/expense.dart';
 
-class ExpensesList extends StatelessWidget {
+class ExpensesList extends StatefulWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoveExpense,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
+  @override
+  ExpensesListState createState() => ExpensesListState();
+}
+
+class ExpensesListState extends State<ExpensesList> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemCount: widget.expenses.length,
+      itemBuilder: (ctx, index) {
+        final expense = widget.expenses[index];
+        return Dismissible(
+          key: ValueKey(expense.id),
+          onDismissed: (direction) {
+            setState(() {
+              widget.expenses.removeAt(index);
+            });
+            widget.onRemoveExpense(expense);
+          },
+          child: ExpenseItem(expense),
+        );
+      },
     );
   }
 }
