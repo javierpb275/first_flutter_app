fdf321e0680e546c9faee8ca3158144b3f1e1226
javierpb275 <javierpb275@gmail.com>
2024-08-04 14:36:39 +0200

    getting started with theming

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(230, 168, 226, 228),
+      ),
       home: const Expenses(),
     ),
   );
