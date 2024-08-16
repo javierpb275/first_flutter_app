0eb3586c38989e489bba263fdf127a65912cc8b4
javierpb275 <javierpb275@gmail.com>
2024-08-16 13:54:43 +0200

    locking device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,20 @@ import 'package:first_flutter_app/widgets/expenses/expenses.dart';
 import 'package:first_flutter_app/widgets/expenses/styles/expenses_dark_theme.dart';
 import 'package:first_flutter_app/widgets/expenses/styles/expenses_light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ExpensesDarkTheme.theme,
-      theme: ExpensesLightTheme.theme,
-      themeMode: ThemeMode.light,
-      home: const Expenses(),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ExpensesDarkTheme.theme,
+        theme: ExpensesLightTheme.theme,
+        themeMode: ThemeMode.light,
+        home: const Expenses(),
+      ),
+    );
+  });
 }
