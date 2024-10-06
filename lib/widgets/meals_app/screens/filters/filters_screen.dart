6d562e04477afd8893ef42c6730b2f2d9f47a310
javierpb275 <javierpb275@gmail.com>
2024-10-06 16:25:22 +0200

    go bnack to pushing screens

--- lib/widgets/meals_app/screens/filters/filters_screen.dart
+++ lib/widgets/meals_app/screens/filters/filters_screen.dart
@@ -1,5 +1,5 @@
-import 'package:first_flutter_app/widgets/meals_app/screens/tabs/tabs_screen.dart';
-import 'package:first_flutter_app/widgets/meals_app/widgets/drawers/main_drawer.dart';
+// import 'package:first_flutter_app/widgets/meals_app/screens/tabs/tabs_screen.dart';
+// import 'package:first_flutter_app/widgets/meals_app/widgets/drawers/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class FiltersScreen extends StatefulWidget {
@@ -17,18 +17,18 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
-      drawer: MainDrawer(
-        onSelectScreen: (identifier) {
-          Navigator.of(context).pop();
-          if (identifier == 'meals') {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (ctx) => const TabsScreen(),
-              ),
-            );
-          }
-        },
-      ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: (identifier) {
+      //     Navigator.of(context).pop();
+      //     if (identifier == 'meals') {
+      //       Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(
+      //           builder: (ctx) => const TabsScreen(),
+      //         ),
+      //       );
+      //     }
+      //   },
+      // ),
       body: Column(
         children: [
           SwitchListTile(
--- lib/widgets/meals_app/screens/tabs/tabs_screen.dart
+++ lib/widgets/meals_app/screens/tabs/tabs_screen.dart
@@ -50,7 +50,7 @@ class _TabsScreenState extends State<TabsScreen> {
   void _setScreen(String identifier) {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).pushReplacement(
+      Navigator.of(context).push(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
