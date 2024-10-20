f890a94cce9798f8963059468da27e210525382c
javierpb275 <javierpb275@gmail.com>
2024-10-20 17:15:05 +0200

    explicit animations: adding animation controller

--- lib/widgets/meals_app/screens/categories/categories_screen.dart
+++ lib/widgets/meals_app/screens/categories/categories_screen.dart
@@ -5,13 +5,38 @@ import 'package:first_flutter_app/widgets/meals_app/screens/meals/meals_screen.d
 import 'package:first_flutter_app/widgets/meals_app/widgets/categories/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where(
           (meal) => meal.categories.contains(category.id),
         )
