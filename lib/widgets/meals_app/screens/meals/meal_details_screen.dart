339b7897c59bd53d639625a00a2a95509fdb6fd8
javierpb275 <javierpb275@gmail.com>
2024-10-20 18:20:46 +0200

    animations: adding multi-screen transitions

--- lib/widgets/meals_app/screens/meals/meal_details_screen.dart
+++ lib/widgets/meals_app/screens/meals/meal_details_screen.dart
@@ -22,11 +22,14 @@ class MealDetailsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14),
             Text(
--- lib/widgets/meals_app/widgets/meals/meal_item.dart
+++ lib/widgets/meals_app/widgets/meals/meal_item.dart
@@ -40,12 +40,15 @@ class MealItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
