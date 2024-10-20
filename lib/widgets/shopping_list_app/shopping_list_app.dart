378cf47f2cb521c1f7b2d16112872f7941943102
javierpb275 <javierpb275@gmail.com>
2024-10-20 19:17:02 +0200

    userinput and forms: building list ui

--- lib/widgets/shopping_list_app/shopping_list_app.dart
+++ lib/widgets/shopping_list_app/shopping_list_app.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/widgets/shopping_list_app/widgets/grocery_list.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingListApp extends StatefulWidget {
@@ -12,6 +13,6 @@ class ShoppingListApp extends StatefulWidget {
 class _ShoppingListAppState extends State<ShoppingListApp> {
   @override
   Widget build(BuildContext context) {
-    return const Text('SHOPPING LIST APP');
+    return const GroceryList();
   }
 }
