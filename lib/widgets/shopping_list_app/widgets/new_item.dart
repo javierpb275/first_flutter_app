6375273d63ca39028893be5f86621b2cdaca69a5
javierpb275 <javierpb275@gmail.com>
2024-10-27 17:54:54 +0100

    userinput and forms: adding validation logic

--- lib/widgets/shopping_list_app/widgets/new_item.dart
+++ lib/widgets/shopping_list_app/widgets/new_item.dart
@@ -28,7 +28,13 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters long.';
+                  }
+                  return null;
                 },
               ),
               Row(
@@ -40,6 +46,15 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(
