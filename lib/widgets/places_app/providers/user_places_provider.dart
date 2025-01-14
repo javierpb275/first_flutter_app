d907690c0b547d5bfab1c629db762d59491f5276
javierpb275 <javierpb275@gmail.com>
2025-01-14 14:14:36 +0100

    Using Native Device features: storing picked image locally. storing place data in a (on-device) sql database. loading data from sql db

--- lib/widgets/places_app/providers/user_places_provider.dart
+++ lib/widgets/places_app/providers/user_places_provider.dart
@@ -2,16 +2,71 @@ import 'dart:io';
 
 import 'package:first_flutter_app/widgets/places_app/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(
+              row['image'] as String,
+            ),
+            location: PlaceLocation(
+              address: row['address'] as String,
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
     final place = Place(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
+
+    final db = await _getDatabase();
+
+    db.insert('user_places', {
+      'id': place.id,
+      'title': place.title,
+      'image': place.image.path,
+      'address': place.location?.address,
+      'lat': place.location?.latitude,
+      'lng': place.location?.longitude,
+    });
+
     state = [place, ...state];
   }
 
